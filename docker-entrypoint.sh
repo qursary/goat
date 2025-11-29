2d300d4492e2d47174082cde812c37daec72135e
ary <35573017+qursary@users.noreply.github.com>
2025-11-29 09:14:38 -0500

    Remove Safe Browsing configuration from entrypoint
    Remove conditional Safe Browsing configuration from nginx.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,9 +5,6 @@ set -e
 # environment variables are set at runtime, so modify the conf only when it starts
 sed -i "s/listen 80/listen $PORT/" /etc/nginx/nginx.conf
 
-if [ ! -z "$SAFE_BROWSING" ]; then
-    sed -i "s/1.1.1.1/1.1.1.3/" /etc/nginx/nginx.conf
-fi
 
 
 # default.conf makes docker listen to 80, and
